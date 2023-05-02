5bca6c9ba802ba3322f91b6ccc9e0c18ea0142bd
Fahad Murtaza <55149341+xerop79int@users.noreply.github.com>
2023-05-02 22:23:55 +0500

    bandleader section has been started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,30 +2,33 @@ import React from 'react';
 import './App.css';
 import Login from './Components/login';
 import Register from './Components/Register';
-import Customer from './Components/Customer/Customer';
-import CustomerRequest from './Components/Customer/CustomerRequest';
-import CustomerRequestedSongs from './Components/Customer/CustomerRequestedSongs';
-import AllCustomersRequests from './Components/Customer/AllCustomersRequests';
-import DisplaySongsList from './Components/Customer/DisplaySongsList';
-import DisplayLikesCount from './Components/Customer/DisplayLikesCount';
 import NowPlaying from './Components/Customer/NowPlaying';
+import BandLeaderDashboard from './Components/Bandleader/BandLeaderDashboard';
+// import Customer from './Components/Customer/Customer';
+// import CustomerRequest from './Components/Customer/CustomerRequest';
+// import CustomerRequestedSongs from './Components/Customer/CustomerRequestedSongs';
+// import AllCustomersRequests from './Components/Customer/AllCustomersRequests';
+// import DisplaySongsList from './Components/Customer/DisplaySongsList';
+// import DisplayLikesCount from './Components/Customer/DisplayLikesCount';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
+  const [customer, setCustomer] = React.useState(true);
+  const [bandleader, setBandleader] = React.useState(false);
   return (
     <ChakraProvider>
     <Router>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        {/* <Route path='/' element={<Customer />} /> */}
-        <Route path='/request' element={<CustomerRequest />} />
+        { customer ? <Route path='/' element={<NowPlaying />} /> : null }
+        {/* { bandleader ? <Route path='/' element={<BandLeaderDashboard />} /> : null } */}
+        {/* <Route path='/request' element={<CustomerRequest />} />
         <Route path='/requested' element={<CustomerRequestedSongs />} />
         <Route path='/allrequests' element={<AllCustomersRequests />} />
         <Route path='/allsongs' element={<DisplaySongsList />} />
-        <Route path='/likes' element={<DisplayLikesCount />} />
-        <Route path='/' element={<NowPlaying />} />
+        <Route path='/likes' element={<DisplayLikesCount />} /> */}
         {/* <Route path='/' element={<Venues />} />
         <Route path='/venues/:Venue_name' element={<Venue />} />
         <Route path='/venues/:Venue_name/submit' element={<SubmitPaper />} />
