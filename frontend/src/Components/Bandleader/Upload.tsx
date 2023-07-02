8af05225f4b09495c8f5a22e8792e9eb41aaa224
Fahad Murtaza <55149341+xerop79int@users.noreply.github.com>
2023-07-03 01:25:13 +0500

    implementing the pdf to txt

--- frontend/src/Components/Bandleader/Upload.tsx
+++ frontend/src/Components/Bandleader/Upload.tsx
@@ -3,19 +3,41 @@ import './Upload.css';
 import axios from 'axios';
 
 const Upload: React.FC = () => {
-    const [file, setFile] = useState<File>();
+    const [file, setFile] = useState<File[]>([]);
 
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if(e.target.files){
-            setFile(e.target.files[0]);
+            const filesArray = Array.from(e.target.files) as File[];
+            setFile(filesArray);
         }
     }
 
-    const handlesubmit = () => {
+    const handlesubmitdatafile = () => {
         if(file){
             const formData = new FormData();
-            formData.append('file', file);
+            formData.append('type', 'data file');
+            file.forEach((file) => {
+                formData.append('file', file);
+            })
+
+            axios.post('http://localhost:8000/upload', formData, {
+                headers: { Authorization: `Token ${localStorage.getItem('token')}` },
+            })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => console.log(err))
+        }
+    }
+
+    const handlesubmitPDFs = () => {
+        if(file){
+            const formData = new FormData();
+            formData.append('type', 'pdf');
+            file.forEach((file) => {
+                formData.append('files', file);
+            })
 
             axios.post('http://localhost:8000/upload', formData, {
                 headers: { Authorization: `Token ${localStorage.getItem('token')}` },
@@ -28,16 +50,20 @@ const Upload: React.FC = () => {
     }
 
 
+
     return(
         <div className="upload">
             <p className="upload-list-heading">Upload The Songs List</p>
             <div className="main-upload-div">
                 <p className="file-select">Select a file</p>
                 <i className="fa-solid fa-download fa-3x"></i>
-                <label className="uplaod-file-lable">Select file or drag here</label>
-                <input onChange={handleFileChange} type="file" accept='text/*' className="uplaod-file" required placeholder="Select A File " />
+                <label className="uplaod-file-lable">Select file(s) <br/>(Instructions: First upload the data file of the songs After they have been uploaded then upload all the PDFs)</label>
+                <input onChange={handleFileChange} type="file" multiple className="uplaod-file" required />
             </div>
-            <button onClick={handlesubmit} className="submit-file">Submit the file</button>
+            <div className='upload-button-container'>
+            <button onClick={handlesubmitdatafile} className="submit-file">Submit Data File</button>
+            <button onClick={handlesubmitPDFs} className="submit-file">Submit PDFs</button>
+            </div>
         </div>
     )
 }
