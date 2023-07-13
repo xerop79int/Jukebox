7ed8f557ed1960f0817cdd75b40f2b9e20e62290
Fahad Murtaza <55149341+xerop79int@users.noreply.github.com>
2023-07-14 02:37:35 +0500

    updated the settings.py file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,10 +137,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000'
-]
+CORS_ORIGIN_WHITELIST = ['*']
+
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000'
+# ]
 
 CORS_ALLOW_HEADERS = [
     'accept',
