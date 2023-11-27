6dbf9f5af10c94effbffc47e31cb971b0da5b90c
Fahad Murtaza <55149341+xerop79int@users.noreply.github.com>
2023-11-27 22:41:38 +0500

    updating the navbar

--- frontend/src/Components/Bandleader/NavbarAdminPortal.tsx
+++ frontend/src/Components/Bandleader/NavbarAdminPortal.tsx
@@ -5,11 +5,11 @@ const NavbarAdminPortal = () => {
     return(
         <div className="admin-song-request-navbar">
             <div className="admin-song-request-nav-links">
-                <a href="/bandleader">Bandleader Dashboard</a>
+                <a href="/bandleader">Dashboard</a>
                 <a href="/addsinglesong">Add Song</a>
                 <a href="/editsong">Edit Song</a>
                 <a href="/addvenue">Venues</a>
-                <a href="/upload">Upload</a>
+                <a href="/upload">Upload Master File</a>
                 <a href="/backup">Backup</a>
                 <a href="/load">Load</a>
                 <a href="/show">Shows</a>
