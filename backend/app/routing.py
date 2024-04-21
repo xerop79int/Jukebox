b38122a7b5f82a71e78f9547103e7dd4650bf5ae
Fahad Murtaza <55149341+xerop79int@users.noreply.github.com>
2024-04-21 15:51:42 +0500

    Delete backend directory

--- backend/app/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-from app import consumers
-
-
-websocket_urlpatterns = [
-    re_path(r'ws/bandleadercustomerrequests/$', consumers.BandLeaderConsumer.as_asgi()),
-    re_path(r'ws/customerrequestsresponse/$', consumers.CustomerConsumer.as_asgi()),
-    re_path(r'ws/bandmember/$', consumers.BandMemberConsumer.as_asgi())
-]
