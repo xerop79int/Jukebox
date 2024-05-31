aa8bd6e2838f8cc9c6cb0ff6eb5ec97d100cc6b6
Fahad Murtaza <55149341+xerop79int@users.noreply.github.com>
2024-05-31 19:58:18 +0500

    updating the code.

--- Backend/app/views/BandleaderViews.py
+++ Backend/app/views/BandleaderViews.py
@@ -1466,8 +1466,7 @@ class ManagerBackupView(APIView):
         
         # check if the backup folder exists
         if(os.path.join(prev_django_dir, 'JukeBox_backup')):
-            print("HEllo")
-            os.system(f'sudo rm -rf {os.path.join(prev_django_dir, 'JukeBox_backup', filename)}')
+            os.system(f'rm -rf {os.path.join(prev_django_dir, 'JukeBox_backup', filename)}')
         
             return Response({'success': 'Backup deleted successfully'})
         else:
